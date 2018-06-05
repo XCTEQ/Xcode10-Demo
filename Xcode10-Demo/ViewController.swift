89e5734ebf0d4b6600213d53882381874e662d49
Shashikant86 <shashikant.jagtap@icloud.com>
2018-06-05 18:52:22 +0100

    Added initial commit for Xcode 10 demo

---
+++ Xcode10-Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Xcode10-Demo
+//
+//  Created by Shashikant Jagtap on 05/06/2018.
+//  Copyright © 2018 Shashikant Jagtap. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ Xcode10-DemoTests/Xcode10_DemoTests.swift
@@ -0,0 +1,36 @@
+//
+//  Xcode10_DemoTests.swift
+//  Xcode10-DemoTests
+//
+//  Created by Shashikant Jagtap on 05/06/2018.
+//  Copyright © 2018 Shashikant Jagtap. All rights reserved.
+//
+
+import XCTest
+@testable import Xcode10_Demo
+
+class Xcode10_DemoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
